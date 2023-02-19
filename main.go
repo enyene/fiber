b328f24ea0b295879d6d96f0269cc599976c65b2
iEnyene <akpanenyene@gmail.com>
2023-02-19 21:36:09 +0100

    initial commit

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+type Details struct {
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	IsPresent bool   `json:"present"`
+}
 
 func main() {
 	fmt.Println("welcome")
@@ -30,5 +37,16 @@ func main() {
 	app.Get("/:name?", func(c *fiber.Ctx) error {
 		return c.SendString("hello" + c.Params("name"))
 	})
+
+	app.Post("/v2/details", func(c *fiber.Ctx) error {
+		detail := new(Details)
+		err := c.BodyParser(detail)
+		if err != nil {
+			return err
+		}
+		log.Println(detail.Name)
+		return c.SendString("details found")
+	})
+
 	app.Listen(":3000")
 }
